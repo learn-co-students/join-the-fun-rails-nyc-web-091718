66a63238d78c7971a016af6dd688ed8c7cfbac8c
srscalia <shelby.scalia@gmail.com>
2018-10-16 17:56:24 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Taxi.create
+Taxi.create
+Taxi.create
+Taxi.create
+Taxi.create
+Passenger.create
+Passenger.create
+Passenger.create
+Passenger.create
+Ride.create(1,1)
